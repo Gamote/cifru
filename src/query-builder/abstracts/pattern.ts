b4072f7080c47e73260fd303c8ff6b6b46405622
Gamote <contatc@gamote.ro>
2023-08-25 01:14:54 +0200

    fix: no `undefined` allowed for type `Name = string | undefined`

--- src/query-builder/abstracts/pattern.ts
+++ src/query-builder/abstracts/pattern.ts
@@ -22,7 +22,15 @@ export abstract class Pattern<
   ) {
     super(priorElement);
 
-    this._name = name;
+    // We are using a type assertion here to satisfy TypeScript's strict type checking.
+    // The problem arises because `name` is an optional parameter that can be `undefined`,
+    // while `this._name` is of type `Name`. TypeScript's type system cannot guarantee that
+    // `Name` can safely accept `undefined` even though we understand that it should be
+    // acceptable in our use case.
+    // Using the type assertion `(undefined as Name)` here is a temporary workaround to
+    // bypass the error.
+    // TODO: Consider revisiting this with future TypeScript versions or design changes to avoid type casting.
+    this._name = name ?? (undefined as Name);
     this._labels = labels ?? [];
   }
 }
