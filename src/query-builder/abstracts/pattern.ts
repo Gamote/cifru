e4f1ef4a5c615a156bca1472ba5d6219171311f1
Gamote <contatc@gamote.ro>
2023-08-25 19:00:41 +0200

    chores: remove type workaround

--- src/query-builder/abstracts/pattern.ts
+++ src/query-builder/abstracts/pattern.ts
@@ -13,7 +13,7 @@ export type ValidPatternName = `${
 export abstract class Pattern<
   Name extends Optional<ValidPatternName> = Optional<ValidPatternName>,
 > extends CloneableElement<Pattern<Name>> {
-  public readonly name: Name;
+  public readonly name?: Name;
   public readonly labelList: string[];
 
   protected constructor(
@@ -23,15 +23,7 @@ export abstract class Pattern<
   ) {
     super(priorElement);
 
-    // We are using a type assertion here to satisfy TypeScript's strict type checking.
-    // The problem arises because `name` is an optional parameter that can be `undefined`,
-    // while `this._name` is of type `Name`. TypeScript's type system cannot guarantee that
-    // `Name` can safely accept `undefined` even though we understand that it should be
-    // acceptable in our use case.
-    // Using the type assertion `(undefined as Name)` here is a temporary workaround to
-    // bypass the error.
-    // TODO: Consider revisiting this with future TypeScript versions or design changes to avoid type casting.
-    this.name = name ?? (undefined as Name);
+    this.name = name;
     this.labelList = labels ?? [];
   }
 }
