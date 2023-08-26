7fe291db45469ce6ea71846377fc34c6b58eef1d
Gamote <contatc@gamote.ro>
2023-08-26 17:04:42 +0200

    fix: tests to support new `attributes`

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -12,53 +12,83 @@ describe('Integration tests', () => {
   });
 
   it('MATCH (u)', () => {
-    expect(c.match(c.node('u')).query()).toBe('MATCH (u)');
+    expect(
+      c
+        .match(
+          c.node({
+            variable: 'u',
+          }),
+        )
+        .query(),
+    ).toBe('MATCH (u)');
   });
 
   it('MATCH (:User)', () => {
-    expect(c.match(c.node().label('User')).query()).toBe('MATCH (:User)');
+    expect(c.match(c.node({ labels: ['User'] })).query()).toBe('MATCH (:User)');
   });
 
   it('MATCH (u:User)', () => {
-    expect(c.match(c.node('u').label('User')).query()).toBe('MATCH (u:User)');
+    expect(
+      c
+        .match(
+          c.node({
+            variable: 'u',
+            labels: ['User'],
+          }),
+        )
+        .query(),
+    ).toBe('MATCH (u:User)');
   });
 
   it('MATCH (u:Provider|Client)', () => {
-    expect(c.match(c.node('u').label('Provider').label('Client')).query()).toBe(
-      'MATCH (u:Provider|Client)',
-    );
+    expect(
+      c
+        .match(
+          c.node({
+            variable: 'u',
+            labels: ['Provider', 'Client'],
+          }),
+        )
+        .query(),
+    ).toBe('MATCH (u:Provider|Client)');
   });
 
   it('MATCH (u:User|Group|Post)', () => {
-    expect(c.match(c.node('u').labels('User', 'Group', 'Post')).query()).toBe(
-      'MATCH (u:User|Group|Post)',
-    );
-  });
-
-  it('After cloning: (u:Like|Comment|Image)', () => {
-    const userNode = c.node('u');
-    userNode.label('user');
-    userNode.labels('group', 'post');
-
-    const finalPattern = userNode.labels('Like', 'Comment', 'Image');
-
-    expect(finalPattern.query()).toBe('(u:Like|Comment|Image)');
+    expect(
+      c
+        .match(
+          c.node({
+            variable: 'u',
+            labels: ['User', 'Group', 'Post'],
+          }),
+        )
+        .query(),
+    ).toBe('MATCH (u:User|Group|Post)');
   });
 
   it('MATCH (u:User) RETURN u', () => {
-    expect(c.match(c.node('u').label('User')).return('u').query()).toBe(
-      'MATCH (u:User) RETURN u',
-    );
+    expect(
+      c
+        .match(
+          c.node({
+            variable: 'u',
+            labels: ['User'],
+          }),
+        )
+        .return('u')
+        .query(),
+    ).toBe('MATCH (u:User) RETURN u');
   });
 
   it(`MATCH (u:User {name: 'John', isActive: true, age: 30}) RETURN u`, () => {
     expect(
       c
         .match(
-          c
-            .node('u')
-            .label('User')
-            .props({ name: 'John', isActive: true, age: 30 }),
+          c.node({
+            variable: 'u',
+            labels: ['User'],
+            properties: { name: 'John', isActive: true, age: 30 },
+          }),
         )
         .return('u')
         .query(),
