5f8a1151c43c89d207889228b39cfc478c9e53e6
Gamote <contatc@gamote.ro>
2024-01-12 20:30:57 +0100

    Refactor ValidateShape type to ExactShape
    The ValidateShape type has been renamed to ExactShape and has been refactored to allow empty objects as valid inputs. The refactoring is helpful in scenarios when `Input` is an extension of `Shape` for value generics, ensuring that it shares the same keys as `Shape`.

--- src/query-builder/type-utils.ts
+++ src/query-builder/type-utils.ts
@@ -28,6 +28,14 @@ export type Alphabet =
 
 export type Optional<T> = T | undefined;
 
-export type ValidateShape<Input, Shape> = {
-  [Key in keyof Input]: Key extends keyof Shape ? Input[Key] : never;
-};
+/**
+ * Useful when the `Input` is an extension of `Shape` for value generics,
+ * and we want to make sure that it has the same keys as `Shape`.
+ *
+ * It allows an empty object `{}` as a valid input.
+ */
+export type ExactShape<Input, Shape> = keyof Input extends never
+  ? Input
+  : {
+      [Key in keyof Input]: Key extends keyof Shape ? Input[Key] : never;
+    };
